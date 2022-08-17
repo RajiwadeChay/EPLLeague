6c35c14c45cf4af30a9b9e76f447757444553f76
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-08-17 18:13:03 +0530

    tab functionality done

--- assets/js/loginpage.js
+++ assets/js/loginpage.js
@@ -1,10 +1,21 @@
-/* Author: 
-
-*/
-
+var tabs = document.querySelectorAll(".tab-list li");
+var tabContent = document.querySelectorAll(".tab-content");
 
 
+tabs.forEach(function (item, idx) {
+  item.addEventListener("click", function () {
+    removeClass();
+    this.classList.add("active-tab");
+    tabContent[idx].classList.add("show");
+  });
+});
 
+function removeClass() {
+  tabs.forEach(function (item, idx) {
+    item.classList.remove("active-tab");
+    tabContent[idx].classList.remove("show");
+  });
+}
 
 
 
