c4c691342c14cd12eb21d2e414961fb65aaf3933
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-08-19 18:21:18 +0530

    Slider js done

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,14 @@
-var uName = document.querySelector(".uName");
-var logOutBtn = document.querySelector(".logout-btn");
+var logOutBtn = document.querySelector(".logout a");
 let uEmail = localStorage.getItem('uEmail') ? localStorage.getItem('uEmail') : '';
 
+const readMoreArr = document.querySelectorAll("a[title='Read More']");
+
+
+//logout event listener
 logOutBtn.addEventListener("click", Logout);
 
-
 if (uEmail == '') {
-  alert('U need to login first');
+  alert('You need to login first!');
   location.href = "loginpage.html";
 }
 function Logout() {
@@ -16,4 +18,39 @@ function Logout() {
   location.href = "loginpage.html";
 }
 
-uName.textContent = localStorage.getItem('fName');
+readMoreArr.forEach(function (item) {
+  item.addEventListener("click", function () {
+    let parentDiv = item.parentElement.parentElement;
+    parentDiv.classList.toggle("expand");
+    if (parentDiv.classList.contains("expand")) {
+      item.textContent = "Read Less";
+    } else {
+      item.textContent = "Read More";
+    }
+  })
+});
+
+const sliderList = document.querySelector(".slider-container ul");
+const slidesArr = Array.from(sliderList.children);
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const carousel = document.querySelector(".carousel");
+const dotArr = Array.from(carousel.children);
+let count = 1;
+
+slideShow();
+function slideShow() {
+  slidesArr.forEach(function (item, idx) {
+    item.classList.remove("show");
+    item.classList.remove("fade");
+    dotArr[idx].classList.remove("active-dot");
+  });
+  count++;
+  if (count > slidesArr.length) { count = 1 }
+  slidesArr[count - 1].classList.add("show");
+  slidesArr[count - 1].classList.add("fade");
+  dotArr[count - 1].classList.add("active-dot");
+  setTimeout(slideShow, 3000); // for caaling afetr each 3sec
+}
+
+
